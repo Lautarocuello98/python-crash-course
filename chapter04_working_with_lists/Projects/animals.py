c308b7f5e5d18eddab4ae4c9e6cda3e6b9697691
Lautarocuello98 <lac1998123@gmail.com>
2026-02-21 12:24:46 -0600

    Add chapter 04

---
+++ chapter04_working_with_lists/Projects/animals.py
@@ -0,0 +1,11 @@
+animals = ['dog', 'cat', 'rabbit']
+
+for animal in animals:
+    print(animal)
+
+print()
+
+for animal in animals:
+    print(f"A {animal} would make a great pet.")
+
+print("\n Any of these animals would make a great pet!")
---
+++ chapter04_working_with_lists/Projects/pizzas.py
@@ -0,0 +1,6 @@
+pizzas = ['pepperoni', 'margarita', 'hawaian']
+
+for pizza in pizzas:
+    print(f"I love the pizza of {pizza}")
+
+print("\nI really love pizza")
