a95a084ddc5caab07b8a30faa5376a77c1de74cd
Lautarocuello98 <lac1998123@gmail.com>
2026-02-21 15:28:18 -0600

    Add chapter 05

---
+++ chapter05_if_statements/Projects/alien_colours.py
@@ -0,0 +1,8 @@
+alien_colour = "green"
+
+if alien_colour == "green":
+    print("You earned 5 points.")
+elif alien_colour == "yellow":
+    print("You earned 10 points.")
+elif alien_colour == "red":
+    print("You earned 15 points.")
---
+++ chapter05_if_statements/Projects/favorite_fruits.py
@@ -0,0 +1,12 @@
+favorite_fruits = ['banana', 'apple', 'strawberry']
+
+if 'banana' in favorite_fruits:
+    print("You really like bananas!")
+if 'apple' in favorite_fruits:
+    print("You really like apples!")
+if 'orange' in favorite_fruits:
+    print("You really like oranges!")
+if 'strawberry' in favorite_fruits:
+    print("You really like strawberries!")
+if 'mango' in favorite_fruits:
+    print('You really like mangos!')
