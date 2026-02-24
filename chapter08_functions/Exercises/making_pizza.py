9c843dc8d33346cf55130d51b04e275cafe50407
Lautarocuello98 <lac1998123@gmail.com>
2026-02-24 16:08:38 -0600

    Add chapter 08

---
+++ chapter08_functions/Exercises/making_pizza.py
@@ -0,0 +1,8 @@
+import pizza
+# or from pizza import make_pizza
+# or from pizza import make_pizza as mp
+# or import pizza as p
+# from pizza impor *
+
+pizza.make_pizza(16, 'pepperoni')
+pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
