bae71e264ad61f42dd19df5e0e844b8319a9df0c
Lautarocuello98 <lac1998123@gmail.com>
2026-02-27 14:45:34 -0600

    Add chapter 09

--- chapter09_classes/Exercises/my_electric_Car.py
+++ chapter09_classes/Exercises/my_electric_Car.py
@@ -1,6 +1,13 @@
-from electric_car import ElectricCar
+from electric_car import ElectricCar, Car
 
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
 my_leaf.battery.describe_batery()
 my_leaf.battery.get_range()
+
+print()
+
+my_mustang = Car('ford', 'mustang', 2024)
+print(my_mustang.get_descriptive_name())
+my_mustang.odometer_reading = 23
+my_mustang.read_odometer()
