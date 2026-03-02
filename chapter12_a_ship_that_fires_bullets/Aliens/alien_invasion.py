46c88740706f369bc9c0880452c21f3b8b938d98
Lautarocuello98 <lac1998123@gmail.com>
2026-03-02 16:59:36 -0600

    Add chapter 12

--- chapter12_a_ship_that_fires_bullets/Aliens/alien_invasion.py
+++ chapter12_a_ship_that_fires_bullets/Aliens/alien_invasion.py
@@ -29,9 +29,20 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
             self.clock.tick(60)
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        # Update bullet positions.
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
 
     def _update_screen(self):
         # Redraw the screen during each pass through the loop.
@@ -74,8 +85,9 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullet_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
 
 if __name__ == '__main__':
