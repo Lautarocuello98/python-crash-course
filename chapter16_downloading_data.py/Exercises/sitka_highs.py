f358e94e7dc65f07376db141a6a4fb7dec58d526
Lautarocuello98 <lac1998123@gmail.com>
2026-03-04 14:43:46 -0600

    Add chapter 16

---
+++ chapter16_downloading_data.py/Exercises/sitka_highs.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import csv
+
+import matplotlib.pyplot as plt
+
+path = Path('C:/Users/lac_/programacion/python crash course/chapter16_downloading_data.py/sitka_weather_07-2021_simple.csv')
+lines = path.read_text(encoding='utf-8').splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+
+# Extract high temperatures.
+highs = []
+for row in reader:
+    high = int(row[4])
+    highs.append(high)
+
+# Plot the high temperatures.
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(highs, color='red')
+
+# Format plot.
+ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
+ax.set_xlabel('Days', fontsize=16)
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(labelsize=16)
+
+
+plt.show()
