0f665c01ef3656f54a982fe126a8b1fc97a466d4
Lautarocuello98 <lac1998123@gmail.com>
2026-03-09 15:03:48 -0600

    Add chapter 20

--- chapter20_styling_and_deploying_an_app/Exercises/learning_log/learning_logs/models.py
+++ chapter20_styling_and_deploying_an_app/Exercises/learning_log/learning_logs/models.py
@@ -11,11 +11,12 @@ class Topic(models.Model):
         """Return a string representation of the model."""
         return self.text
 
+
 class Entry(models.Model):
-    """"Something specific learned about a topic."""
+    """Something specific learned about a topic."""
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
-    data_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name_plural = 'entries'
